a513db6477abf65bf5805fe5a09f24043e94b738
Natalie Wojciechowski <Shippolvr@yahoo.com>
2019-05-30 21:18:27 -0500

    continuing basic functionality with show/edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,45 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
